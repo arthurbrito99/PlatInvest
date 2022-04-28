317c4f1e4918037adc186115536bf3d926731e9f
arthurbrito99 <arthur.brito1999@outlook.com>
2022-04-28 00:17:24 -0300

    Start Project

---
+++ src/main/java/com/arthurb/PlatInvest/PlatInvestApplication.java
@@ -0,0 +1,13 @@
+package com.arthurb.PlatInvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatInvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurb/PlatInvest/PlatInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurb.PlatInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

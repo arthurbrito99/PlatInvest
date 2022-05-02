d1114765788b26da24594a2591804c6b8b16e309
arthurbrito99 <arthur.brito1999@outlook.com>
2022-05-02 18:27:29 -0300

    Company Controller and Repository

--- src/main/java/com/arthurb/PlatInvest/controller/CompanyController.java
+++ src/main/java/com/arthurb/PlatInvest/controller/CompanyController.java
@@ -4,10 +4,8 @@ import com.arthurb.PlatInvest.model.Company;
 import com.arthurb.PlatInvest.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:8080")
@@ -16,8 +14,49 @@ import java.util.List;
 public class CompanyController {
     @Autowired
     CompanyRepository companyRepository;
+
+    @PostMapping("/company")
+    public ResponseEntity<Company> createCompany(@Validated @RequestBody Company company){
+        try {
+            return ResponseEntity.ok(companyRepository.save(company));
+        }   catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/company/{id}")
+    public ResponseEntity<Company> updateCompany(@PathVariable("id") Long id, @RequestBody Company company){
+        Company auxCompany = companyRepository.findById(id).orElse(null);
+        if(auxCompany != null){
+            auxCompany.setStock(company.getStock());
+            auxCompany.setTicker(company.getTicker());
+            auxCompany.setPrice(company.getPrice());
+            auxCompany.setStatus(company.getStatus());
+            return ResponseEntity.ok(companyRepository.save(auxCompany));
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/company/{id}")
+    public ResponseEntity<Company> deleteCompany(@PathVariable("id") Long id){
+        if(companyRepository.existsById(id)){
+            companyRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
     @GetMapping("/company")
     public ResponseEntity<List<Company>> getAllCompany(){
         return ResponseEntity.ok(companyRepository.findAll());
+    }
+
+    @GetMapping("/company/status")
+    public ResponseEntity<List<Company>> getAllCompanyByStatus(@RequestParam("status") Boolean status){
+        return ResponseEntity.ok(companyRepository.findByStatus(status));
     }
 }

c565d0fb9fa9f0d83a53ae44f6b59bf1aaf59822
arthurbrito99 <arthur.brito1999@outlook.com>
2022-05-04 00:43:22 -0300

    Investment Structure

---
+++ src/main/java/com/arthurb/PlatInvest/model/InvestmentReceipt.java
@@ -0,0 +1,60 @@
+package com.arthurb.PlatInvest.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "investment_receipt")
+public class InvestmentReceipt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "investmentRecord_id")
+    private InvestmentRecord investmentRecord;
+
+    @OneToOne
+    @JoinColumn(name = "company_id", referencedColumnName = "ID")
+    private Company company;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "total")
+    private Double total;
+
+    public InvestmentReceipt() {
+    }
+
+    public InvestmentReceipt(Company company, Integer quantity, Double total) {
+        this.company = company;
+        this.quantity = quantity;
+        this.total = total;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+}
---
+++ src/main/java/com/arthurb/PlatInvest/model/InvestmentRecord.java
@@ -0,0 +1,55 @@
+package com.arthurb.PlatInvest.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "investment_record")
+public class InvestmentRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "investmentRecord", cascade = CascadeType.ALL)
+    private List<InvestmentReceipt> receipt = new ArrayList<>();
+
+    @Column(name = "total")
+    private Double total;
+
+    @Column(name = "change")
+    private Double change;
+
+    public InvestmentRecord(){
+    }
+
+    public InvestmentRecord(List<InvestmentReceipt> receipt, Double total, Double change) {
+        this.receipt = receipt;
+        this.total = total;
+        this.change = change;
+    }
+
+    public List<InvestmentReceipt> getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(List<InvestmentReceipt> receipt) {
+        this.receipt = receipt;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getChange() {
+        return change;
+    }
+
+    public void setChange(Double change) {
+        this.change = change;
+    }
+}

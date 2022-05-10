cbbd233d48b9097d8ce9bd3664925e3a2a41b84d
arthurbrito99 <arthur.brito1999@outlook.com>
2022-05-10 20:10:34 -0300

    Application Properties with encrypted key

--- src/main/java/com/arthurb/PlatInvest/PlatInvestApplication.java
+++ src/main/java/com/arthurb/PlatInvest/PlatInvestApplication.java
@@ -1,9 +1,13 @@
 package com.arthurb.PlatInvest;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableEncryptableProperties
 public class PlatInvestApplication {
 
 	public static void main(String[] args) {
